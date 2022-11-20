d0eb5ea0e4f6ea41d5d85dd8d661775a29cb6fee
Charles Frye <charlesfrye@berkeley.edu>
2022-11-19 21:31:16 -0800

    add and apply pre-commit lintng/fmting

--- Makefile
+++ Makefile
@@ -6,13 +6,19 @@ help: ## get a list of all the targets, and their short descriptions
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' Makefile | awk 'BEGIN {FS = ":.*?##"}; {printf "\033[36m%-12s\033[0m %s\n", $$1, $$2}'
 
 
-train: environment ## on modal, finetune a dreambooth model to generate a target instance
+model: environment ## on modal, finetune a dreambooth model to generate a target instance
 	A100=1 MODAL_GPU=1 modal app run run.py --function-name train
 
 
-inference: environment ## run inference on modal, based on the target instance plus a PROMPT_POSTFIX
-	PROMPT_POSTFIX="$(PROMPT_POSTFIX)" A100=1 MODAL_GPU=1 modal app run run.py --function-name infer
+inference: environment ## run inference on modal, based on the target instance, flanked by a prefix and postfix
+	PROMPT_PREFIX="$(PROMPT_PREFIX)" PROMPT_POSTFIX="$(PROMPT_POSTFIX)" A100=1 MODAL_GPU=1 modal app run run.py --function-name infer
 
 
 environment: ## install local requirements
 	pip install -r requirements-local.txt
+
+pyfmt: dev-environment ## run formatting
+	pre-commit run --all-files black
+
+dev-environment: environment ## install local dev requirements
+	pip install pre-commit
--- run.py
+++ run.py
@@ -11,13 +11,17 @@ import modal
 #  change the entries there to change the project
 dotenv.load_dotenv()
 
+
 @dataclass
 class ProjectConfig:
     """Project-level configuration information, provided locally."""
+
     PROJECT_NAME: str = os.environ.get("PROJECT_NAME")
     IMAGES_FILE_URL: str = os.environ.get("IMAGES_FILE_URL")
     INSTANCE_PREFIX: str = os.environ.get("INSTANCE_PREFIX")
     INSTANCE_PHRASE: str = os.environ.get("INSTANCE_PHRASE")
+    INSTANCE_POSTFIX: str = os.environ.get("INSTANCE_POSTFIX")
+
 
 project_config = ProjectConfig()
 stub = modal.Stub(name=project_config.PROJECT_NAME)
@@ -51,6 +55,7 @@ IMG_PATH = Path(__file__).parent / "img"
 @dataclass
 class TrainConfig:
     """Hyperparameters/constants from the huggingface training repo."""
+
     resolution: int = 512
     train_batch_size: int = 1
     gradient_accumulation_steps: int = 1
@@ -68,8 +73,8 @@ class TrainConfig:
     timeout=480,
     interactive=True,
     secrets=[modal.Secret.from_name("huggingface"), stub["local_config"]],
-    )
-def train(config = TrainConfig()):
+)
+def train(config=TrainConfig()):
     from accelerate.utils import write_basic_config
     import huggingface_hub
 
@@ -89,28 +94,31 @@ def train(config = TrainConfig()):
     script_path = "examples/dreambooth/train_dreambooth.py"
     script_url = f"{raw_repo_url}/{script_commit_hash}/{script_path}"
 
-    subprocess.run(
-        ["wget", script_url, "-O", "train_dreambooth.py"]
+    subprocess.run(["wget", script_url, "-O", "train_dreambooth.py"])
+
+    instance_prefix, instance_postfix = map(
+        os.environ.get, ("INSTANCE_PREFIX", "INSTANCE_POSTFIX")
     )
-
-    instance_prefix, instance_postfix = os.environ["instance_prefix"], os.environ["instance_postfix"]
-    instance_phrase = os.environ["instance_phrase"]
-    prompt = f"{instance_prefix} {instance_phrase} {instance_postfix}"
+    instance_phrase = os.environ["INSTANCE_PHRASE"]
+    prompt = f"{instance_prefix} {instance_phrase} {instance_postfix}".strip()
 
     subprocess.run(
-        ["accelerate", "launch", "train_dreambooth.py",
-        f"--pretrained_model_name_or_path={MODEL_NAME}",
-        f"--instance_data_dir={INSTANCE_DIR}",
-        f"--output_dir={OUTPUT_DIR}",
-        f"--instance_prompt='{prompt}'",
-        f"--resolution={config.resolution}",
-        f"--train_batch_size={config.train_batch_size}",
-        f"--gradient_accumulation_steps={config.gradient_accumulation_steps}",
-        f"--learning_rate={config.learning_rate}",
-        f"--lr_scheduler={config.lr_scheduler}",
-        f"--lr_warmup_steps={config.lr_warmup_steps}",
-        f"--max_train_steps={config.max_train_steps}",
-    ]
+        [
+            "accelerate",
+            "launch",
+            "train_dreambooth.py",
+            f"--pretrained_model_name_or_path={MODEL_NAME}",
+            f"--instance_data_dir={INSTANCE_DIR}",
+            f"--output_dir={OUTPUT_DIR}",
+            f"--instance_prompt='{prompt}'",
+            f"--resolution={config.resolution}",
+            f"--train_batch_size={config.train_batch_size}",
+            f"--gradient_accumulation_steps={config.gradient_accumulation_steps}",
+            f"--learning_rate={config.learning_rate}",
+            f"--lr_scheduler={config.lr_scheduler}",
+            f"--lr_warmup_steps={config.lr_warmup_steps}",
+            f"--max_train_steps={config.max_train_steps}",
+        ]
     )
 
 
@@ -119,9 +127,11 @@ class InferenceConfig:
     num_inference_steps: int = 50
     guidance_scale: float = 7.5
 
+
 inference_prompts = {
+    "PROMPT_PREFIX": os.environ.get("PROMPT_PREFIX", ""),
     "PROMPT_POSTFIX": os.environ.get("PROMPT_POSTFIX", ""),
-    "DIRECT_PROMPT": os.environ.get("DIRECT_PROMPT", "")
+    "DIRECT_PROMPT": os.environ.get("DIRECT_PROMPT", ""),
 }
 stub["inference_prompts"] = modal.Secret(inference_prompts)
 
@@ -132,20 +142,26 @@ stub["inference_prompts"] = modal.Secret(inference_prompts)
     cpu=1,
     shared_volumes={str(MODEL_DIR): volume},
     timeout=120,
-    secrets=[modal.Secret.from_name("wandb"), stub["inference_prompts"], stub["local_config"]]
-    )
-def infer(config = InferenceConfig()):
+    secrets=[
+        modal.Secret.from_name("wandb"),
+        stub["inference_prompts"],
+        stub["local_config"],
+    ],
+)
+def infer(config=InferenceConfig()):
     from diffusers import StableDiffusionPipeline
     import torch
     import wandb
 
-    pipe = StableDiffusionPipeline.from_pretrained(MODEL_DIR, torch_dtype=torch.float16).to("cuda")
+    pipe = StableDiffusionPipeline.from_pretrained(
+        MODEL_DIR, torch_dtype=torch.float16
+    ).to("cuda")
 
     # create prompt based on dreambooth training instance info
-    instance_prefix = os.environ["INSTANCE_PREFIX"]
+    prompt_prefix = os.environ.get("PROMPT_PREFIX", "")
     instance_phrase = os.environ["INSTANCE_PHRASE"]
     prompt_postfix = os.environ.get("PROMPT_POSTFIX", "")
-    prompt = f"{instance_prefix} {instance_phrase} {prompt_postfix}"
+    prompt = f"{prompt_prefix} {instance_phrase} {prompt_postfix}"
 
     # or over-ride with user-supplied prompt
     prompt = os.environ.get("DIRECT_PROMPT") or prompt
@@ -155,7 +171,12 @@ def infer(config = InferenceConfig()):
 
     wandb.init(project=f"{os.environ['PROJECT_NAME']}", config={"prompt": prompt})
     for _ in range(16):
-        image = pipe(prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale).images[0]
+        image = pipe(
+            prompt,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+        ).images[0]
+
         wandb.log({"generation": wandb.Image(image, caption=prompt)})
     wandb.finish()
 
